61ef96db9922ecfbfb932287bff0c9ff5e3f09d8
Thorsten Sommer <service@sommer-engineering.com>
2020-01-06 22:17:40 +0100

    Made EdPoint public

--- Ed25519/EdPoint.cs
+++ Ed25519/EdPoint.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Ed25519
 {
-    internal struct EdPoint
+    public struct EdPoint
     {
         public BigInteger X { get; set; }
 
@@ -67,7 +67,7 @@ namespace Ed25519
                 };
             }
 
-            var q = this.ScalarMul(e / Constants.TWO);
+            var q = this.ScalarMul(BigInteger.Divide(e / Constants.TWO);
             q = q.EdwardsSquare();
 
             return e.IsEven ? q : q.Edwards(this);
@@ -96,6 +96,11 @@ namespace Ed25519
             var dxxyy = Constants.D * yy * xx;
 
             return (yy - xx - dxxyy - 1).Mod(Constants.Q).Equals(BigInteger.Zero);
+        }
+
+        public override string ToString()
+        {
+            return $"Point(x={this.X}; y={this.Y})";
         }
     }
 }
